a102eb005c7bdfa2802f713951bd024c2e267c22
guro <guramidgebuadze@gmail.com>
2020-11-25 12:39:07 +0400

    imrpove code for data fetching

--- src/App.js
+++ src/App.js
@@ -9,37 +9,47 @@ function App() {
   const [data, setData] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
 
+  const getTypicode = async () => {
+    try {
+      const { data } = await axios.get(
+        'https://jsonplaceholder.typicode.com/posts'
+      );
+      return data;
+    } catch (error) {
+      alert(error);
+    }
+  };
+
   useEffect(() => {
     // Use async/await
-    axios.get('https://jsonplaceholder.typicode.com/posts')
-      .then(({ data }) => {
-        setIsLoading(false);
-        setData(prev => [...prev, ...data]);
-      }).catch(error => console.log(error));
+    getTypicode().then(data => {
+      setIsLoading(false);
+      setData(prev => [...prev, ...data]);
+    });
   }, []);
 
   //Septate component
   const list = () => {
-    return data && data.map(data => {
-      return (
-        <li key={data.id}>
-          <h1>{data.title}</h1>
-          <p>{data.body}</p>
-        </li>
-      )
-    })
-  }
+    return (
+      data &&
+      data.map(data => {
+        return (
+          <li key={data.id}>
+            <h1>{data.title}</h1>
+            <p>{data.body}</p>
+          </li>
+        );
+      })
+    );
+  };
 
   return (
-    <div className="App">
+    <div className='App'>
       <header>
         <h1>კაპოს ცხრილი</h1>
       </header>
       <main>
-        {isLoading ? <Img src={spinner} alt="spinner" /> : <Ul>
-          {list()}
-        </Ul>
-        }
+        {isLoading ? <Img src={spinner} alt='spinner' /> : <Ul>{list()}</Ul>}
       </main>
     </div>
   );
@@ -48,11 +58,11 @@ function App() {
 export default App;
 
 const Ul = styled.ul`
-display: grid;
+  display: grid;
   grid-template-columns: repeat(3, 1fr);
   column-gap: 20px;
-`
+`;
 const Img = styled.img`
   width: 40%;
   height: 40%;
-`
+`;
